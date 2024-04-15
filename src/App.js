b74ff8f0b08a64d7e3cd059364eda56649e9b22f
hieutapcodde <hieuhoang120402dhcn@gmail.com>
2024-04-15 23:41:30 +0700

    thêm user chưa sử dụng api

--- src/App.js
+++ src/App.js
@@ -2,16 +2,33 @@ import { Container, Row } from 'react-bootstrap';
 import './App.scss'
 import Header from './Components/Header';
 import TableUsers from './Components/TableUsers';
+import ModalAddNew from './Components/ModalAddNew';
+import { useState } from 'react';
 function App() {
+
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  }
+
   return (
     <div className='app-container'>
 
       <Header/>
       <Container>
+      <div className="my-3 add-new">
+        <span><b>List User:</b></span>
+        <button className="btn btn-success" onClick={() => setIsShowModalAddNew(true)}>
+          Add new user
+        </button>
+      </div>
       <TableUsers/>
       </Container>
 
-
+      <ModalAddNew
+        show={isShowModalAddNew}
+        handleClose={handleClose}
+      />
 
     </div>
   );
--- src/Components/TableUsers.js
+++ src/Components/TableUsers.js
@@ -5,38 +5,38 @@ import { fetchAllUser } from '../Services/UserServices';
 import ReactPaginate from 'react-paginate';
 
 const TableUsers = (props) => {
-    const [listUsers, setListUsers] = useState([]);
-    const [totalUsers, setTotalUsers] = useState(0);
-    const [totalPages, setTotalPages] = useState(0);
-    useEffect(()=>{
-        //call apis
-        //dry
-        getUsers(1);
-    },[])
+  const [listUsers, setListUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  useEffect(() => {
+    //call apis
+    //dry
+    getUsers(1);
+  }, [])
 
-    const getUsers = async (page) => {
-        let res = await fetchAllUser(page);
-        console.log("check new res: ", res);
+  const getUsers = async (page) => {
+    let res = await fetchAllUser(page);
+    console.log("check new res: ", res);
 
-        if(res && res.data){
-          console.log(res);
-          setTotalUsers(res.total);
-          setListUsers(res.data);
-          setTotalPages(res.total_pages);
-        }
-        console.log(">>> check response: ", res)
+    if (res && res.data) {
+      console.log(res);
+      setTotalUsers(res.total);
+      setListUsers(res.data);
+      setTotalPages(res.total_pages);
     }
+    console.log(">>> check response: ", res)
+  }
 
-        console.log(listUsers);
+  console.log(listUsers);
 
-      const handlePageClick = (event) =>{
-         console.log("event lib: ", event); 
-         //dấu + trước event không biết đưuọc thg event này là string hay số trong trường hợp là string thì convert sang số
-         getUsers(+event.selected + 1);
-      }
+  const handlePageClick = (event) => {
+    console.log("event lib: ", event);
+    //dấu + trước event không biết đưuọc thg event này là string hay số trong trường hợp là string thì convert sang số
+    getUsers(+event.selected + 1);
+  }
 
-    return (<>
-        <Table striped bordered hover>
+  return (<>
+    <Table striped bordered hover>
       <thead>
         <tr>
           <th>ID</th>
@@ -47,39 +47,39 @@ const TableUsers = (props) => {
       </thead>
       <tbody>
         {
-          listUsers && listUsers.length > 0 && 
-          listUsers.map((item,index) => {
-            return (        
-            <tr key={`users-${index}`}>
-              <td>{item.id}</td>
-              <td>{item.email}</td>
-              <td>{item.first_name}</td>
-              <td>{item.last_name}</td>
-            </tr>)
+          listUsers && listUsers.length > 0 &&
+          listUsers.map((item, index) => {
+            return (
+              <tr key={`users-${index}`}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+              </tr>)
           })
         }
       </tbody>
     </Table>
     <ReactPaginate
-        breakLabel="..."
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={totalPages}
-        previousLabel="< previous"
-        
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
-      />
-    </>)
+      breakLabel="..."
+      nextLabel="next >"
+      onPageChange={handlePageClick}
+      pageRangeDisplayed={5}
+      pageCount={totalPages}
+      previousLabel="< previous"
+
+      pageClassName="page-item"
+      pageLinkClassName="page-link"
+      previousClassName="page-item"
+      previousLinkClassName="page-link"
+      nextClassName="page-item"
+      nextLinkClassName="page-link"
+      breakClassName="page-item"
+      breakLinkClassName="page-link"
+      containerClassName="pagination"
+      activeClassName="active"
+    />
+  </>)
 }
 
 export default TableUsers;
